f3736e83578c1d6089f607dc9b81c34b4b75e626
itsme-dazainaki <72004911+itsme-dazainaki@users.noreply.github.com>
2021-12-07 23:33:06 +0700

    Add files via upload

---
+++ ProjectBangun/src/bangundatar/Lingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author Radaza Putri
+ */
+public class Lingkaran extends BangunDatar {
+    
+    public double jarijari;
+     
+    public double hitungLuas(){        
+        double luas = Math.PI * jarijari * jarijari;
+        return luas;
+    }
+    
+    public double hitungKeliling(){ 	  
+        double keliling = 2 * Math.PI * jarijari;
+        return keliling;
+    }
+}
---
+++ ProjectBangun/src/bangundatar/PersegiPanjang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author Radaza Putri
+ */
+public class PersegiPanjang extends BangunDatar {
+
+    public double panjang, lebar;
+     
+    public double hitungLuas(){        
+        double luas = panjang * lebar;
+        return luas;
+    }
+
+    public double hitungKeliling(){ 	  
+        double keliling = 2 * (panjang + lebar);
+        return keliling;
+    }
+}
